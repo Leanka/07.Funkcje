0f9ee7eabcca0b9112040d95d01cba8ecddcefc1
Leanka <anka.lesniak@gazeta.pl>
2017-04-27 00:39:46 +0200

    update funkcji calculate, case 3 i 4

--- Chapter07/ex.10.cpp
+++ Chapter07/ex.10.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -21,21 +22,21 @@ double calculate(double, double, double (*pa)(double, double), double (*ps)(doub
             wynik = calculate(wczytaj(1), wczytaj(2), add, srednia); //NAJPIERW WCZYTUJE 2 PARAMETR, A POTEM PIERWSZY... ?
             cout << "Wynik to " << wynik << endl << endl;
 
-            cout << "Jesli chcesz sprobowac jeszcze raz, wybierz 'q',\n"
+            cout << "Jesli chcesz sprobowac jeszcze raz, wybierz 'y',\n"
                  << "Jesli chcesz zakonczyc, wybierz inny znak.\n"
                  << "WYBIERAM: ";
             cin >> znak;
 
-        }while(znak == 'q');
+        }while(znak == 'y');
 
         return 0;
     }
     void show()
     {
         cout << "\nWybrales dwie liczby. Jesli chcesz, mo¿esz:\n"
-             << "1. dodac je        2. wyliczyc ich srednia\n"
-             << "3. wyjsc\n"
-             << "WYBIERAM: ";
+             << "1. dodac je                     2. wyliczyc ich srednia\n"
+             << "3. ponowanie wczytac liczby     4. wyjsc z programu\n"
+             << "   WYBIERAM: ";
 
     }
     double wczytaj(int i)
@@ -49,6 +50,7 @@ double calculate(double, double, double (*pa)(double, double), double (*ps)(doub
                 continue;
             cout << "\nNiepoprawne dane. Podaj liczbe: ";
         }
+        //cin.get();
         return liczba;
     }
     double add(double a, double b)
@@ -64,15 +66,21 @@ double calculate(double, double, double (*pa)(double, double), double (*ps)(doub
         show();
 
         int choice;
-        while(choice != 3)
+        while(choice != 4)
         {
-            cin >> choice;
+           cin >> choice;
             switch(choice)
             {
                 case 1 : return (*pa)(a, b);
                 case 2 : return (*ps)(a, b);
-                case 3 : cout << "Koniec!"; //zwrocic do maina cos, co zakonczy zamiast wyswietlac wynik
+                case 3 : cin.clear();
+                         while(cin.get() != '\n')
+                            continue;
+                        main(); //dziala bez przekazywania wskaznika na main jako arg funkcji, czyli funkcja zadziala w funkcji i bez przekazywania wskaznika na nia? to po co te wskazniki na funkcje?
+                                //calculate(wczytaj(1), wczytaj(2), add, srednia);      //zwraca wynik dzialania na pierwszych danych, przekazanie wskaznika na calculate jako argumentu funkcji calculate to naprawi?
                          break;
+                case 4 : cout << "Koniec programu!";
+                         exit(EXIT_FAILURE);
                 default : cin.clear();
                           while(cin.get() != '\n')
                             continue;
